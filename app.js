59a54b3d50e2c4135a4b7339b7b71b19c4fa3318
GowrisGit <gowris.dev@gmail.comgit config -global user.name GowrisGitgit config --global user.email gowris.dev@gmail.com>
2020-10-29 23:58:18 +1100

    add button code in app.js

--- app.js
+++ app.js
@@ -44,3 +44,13 @@ const author = document.getElementById("author");
 const job = document.getElementById("job");
 const info = document.getElementById("info");
 
+const prevBtn = document.querySelector('.prev-btn');
+const nextBtn = document.querySelector('.next-btn');
+const randomBtn = document.querySelector('.random-btn');
+
+//set starting item
+let currentItem = 0;
+
+window.addEventListener("DOMContentLoaded", function(){
+  console.log("shake and Bake");
+});
